27920ba78ece36a45ff14ccef032cc87e5d1e5a2
CodrinCarpovici <gabriel_9729@yahoo.co.uk>
2023-12-01 19:57:37 +0000

    added selectors for all important elements from the html files

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,21 @@ import questions from './questions.js';
 // Quiz Variables
 
 // Quiz Elements
+const scores = document.querySelector(".scores");
+const highscores = document.querySelector("#highscores");
+const clearButton = document.querySelector("#clear");
+const timer = document.querySelector(".timer");
+const time = document.querySelector("#time");
+const startScreen = document.querySelector("#start-screen");
 const startButton = document.querySelector("#start");
+const quiz = document.querySelector("#questions");
+const questionTitle = document.querySelector("#question-title");
+const choices = document.querySelector("#choices");
+const endScreen = document.querySelector("#end-screen");
+const finalScore = document.querySelector("#final-score");
+const initials = document.querySelector("#initials");
+const submitButton = document.querySelector("#submit");
+const feedback = document.querySelector("#feedback");
 
 // Function to start the quiz
 const startQuiz = () => {
